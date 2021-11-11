373b768e2ec57ba4b9f044074b9a94a1de2e577b
Zachiah <zachiahsawyer1@gmail.com>
2021-11-11 08:00:37 -0800

    significantly improve ui

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,32 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import PauseIcon from "./lib/icons/PauseIcon.svelte";
+  import PlayIcon from "./lib/icons/PlayIcon.svelte";
+  import ShuffleIcon from "./lib/icons/ShuffleIcon.svelte";
+  import Life from "./lib/Life";
+  import nextGeneration from "./lib/nextGeneration";
+  import ToolbarButton from "./lib/ToolbarButton.svelte";
 
+  const SIZE = 300;
   const SCALE = 10;
 
-  import Life from "./lib/Life";
-
-  import nextGeneration from "./lib/nextGeneration";
-
-  let life = new Life(200, 400);
-  life.randomize();
-
+  const WINDOW_RATIO = window.innerWidth / window.innerHeight;
+  const r = WINDOW_RATIO;
+  const WIDTH = Math.floor(r > 1 ? SIZE : SIZE * r);
+  const HEIGHT = Math.floor(r > 1 ? SIZE / r : SIZE);
   const MAIN_COLOR = "#000";
-  const OTHER_COLORS = ["#666", "#888", "#AAA", "#CCC", "#EEE", "#FFF"]
+  const OTHER_COLORS = ["#666", "#888", "#AAA", "#CCC", "#EEE", "#FFF"];
 
-  let cells = life.cells;
+  let cells = getInitialCells(WIDTH, HEIGHT);
   let pastCells = [cells];
 
   let playing = false;
   let interval = null;
+
+  function getInitialCells(width, height) {
+    let life = new Life(width, height);
+    return life.cells;
+  }
 
   function iterate() {
     nextGeneration(cells).then((s) => {
@@ -34,9 +43,23 @@
     });
   }
 
+  function randomize() {
+    let life = new Life(WIDTH, HEIGHT);
+    life.randomize();
+    cells = life.cells;
+  }
+
   function play() {
     playing = true;
     iterate();
+  }
+
+  function switchPausePlay() {
+    if (playing) {
+      playing = false;
+    } else {
+      play();
+    }
   }
 
   let context;
@@ -49,38 +72,28 @@
 
   $: {
     if (context) {
-      const scale = life.height < life.width ?  canvas.width / life.width : canvas.height / life.height;
-
       context.clearRect(0, 0, canvas.width, canvas.height);
 
-
       for (let [index, pastCellsGrid] of pastCells.entries()) {
-        context.fillStyle = OTHER_COLORS.reverse()[
-          index
-        ];
+        context.fillStyle = OTHER_COLORS.reverse()[index];
         for (let i = 0; i < cells.length; i++) {
           for (let j = 0; j < cells[i].length; j++) {
-            if (
-              pastCellsGrid[i][j]
-            ) {
-              context.fillRect(i * scale, j * scale, scale, scale);
+            if (pastCellsGrid[i][j]) {
+              context.fillRect(i * SCALE, j * SCALE, SCALE, SCALE);
             }
           }
         }
       }
-
 
       context.fillStyle = MAIN_COLOR;
       // draw rect for each live cell
       for (let i = 0; i < cells.length; i++) {
         for (let j = 0; j < cells[i].length; j++) {
           if (cells[i][j]) {
-            context.fillRect(i * scale, j * scale, scale, scale);
+            context.fillRect(i * SCALE, j * SCALE, SCALE, SCALE);
           }
         }
       }
-
-
     }
   }
 
@@ -108,42 +121,21 @@
 {/each} -->
 
 <canvas
-  width="1000"
-  height="1000"
-  class="w-[80vmin] m-auto mt-4"
+  width={WIDTH * SCALE}
+  height={HEIGHT * SCALE}
+  class="w-full h-full absolute"
   style="aspect-ratio: 1/1"
   bind:this={canvas}
 />
 
-<button
-  class="p-4 bg-green-400 text-white mt-8 w-full"
-  on:click={async () => {
-    if (playing) {
-      playing = false;
-    } else {
-      play();
-    }
-  }}
->
-  {playing ? "Stop" : "Play"}
-</button>
-
-<button
-  class="p-4 bg-red-400 text-white mt-8 w-full"
-  on:click={() => {
-    life = new Life(100, 100);
-    life.randomize();
-    cells = life.cells;
-    pastCells = [cells];
-  }}
->
-  Randomize
-</button>
+<div class="flex items-center justify-center absolute bottom-4 w-full">
+  <div class="flex gap-2 bg-blue-400 p-1 rounded-md">
+    <ToolbarButton Icon={ShuffleIcon} on:click={randomize} />
+    <ToolbarButton
+      Icon={playing ? PauseIcon : PlayIcon}
+      on:click={switchPausePlay}
+    />
+  </div>
+</div>
 
 <!--Next button-->
-<button
-  class="p-4 bg-blue-400 text-white mt-8 w-full"
-  on:click={iterate}
->
-  Next
-</button>
---
+++ src/lib/ToolbarButton.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let Icon
+</script>
+
+<button class="p-2 bg-green-400 rounded-full flex items-center justify-center" on:click>
+    <svelte:component this={Icon} />
+</button>
