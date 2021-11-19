bf557dd07ded957691db010af89750a975766f07
Zachiah <zachiahsawyer1@gmail.com>
2021-11-18 16:56:47 -0800

    add functionality for adding objects

--- src/App.svelte
+++ src/App.svelte
@@ -9,12 +9,14 @@
   import {
     BLINKER_SHIP,
     GLIDER,
+    OBJECTS,
     SPACESHIP,
     spinObject90Deg,
   } from "./lib/objects";
   import ToolbarButton from "./lib/ToolbarButton.svelte";
   import ToolbarControl from "./lib/ToolbarControl.svelte";
   import NextIcon from "./lib/icons/NextIcon.svelte";
+  import AddIcon from "./lib/icons/AddIcon.svelte";
 
   let size = 100;
   $: validSize = size || 100;
@@ -26,7 +28,14 @@
   $: WIDTH = Math.floor(r > 1 ? validSize : validSize * r);
   $: HEIGHT = Math.floor(r > 1 ? validSize / r : validSize);
   const MAIN_COLOR = "#000";
-  const OTHER_COLORS = ["#666", "#888", "#AAA", "#CCC", "#EEE", "#FFF"].reverse();
+  const OTHER_COLORS = [
+    "#666",
+    "#888",
+    "#AAA",
+    "#CCC",
+    "#EEE",
+    "#FFF",
+  ].reverse();
 
   let pastCells;
   let cells;
@@ -126,12 +135,16 @@
   let mouseIsDown = false;
 
   const draw = (e: MouseEvent) => {
+    const { x, y } = getDrawCoordinates(e);
+
+    cells[x][y] = !eraser;
+    cells = cells;
+  };
+
+  const getDrawCoordinates = (e: MouseEvent) => {
     const target = e.target as HTMLCanvasElement;
 
     const theScale = target.getBoundingClientRect().width / cells.length;
-    console.log(e.target);
-    console.log(theScale);
-
     const x = Math.floor(
       (e.clientX - target.getBoundingClientRect().left) / theScale
     );
@@ -139,29 +152,42 @@
       (e.clientY - target.getBoundingClientRect().top) / theScale
     );
 
-    console.log(x, y);
-    cells[x][y] = !eraser;
-    cells = cells;
+    return { x, y };
   };
 
   let eraser = false;
+
+  let objectAddingOpen = false;
+
+  const showObject = (object: boolean[][]) => () => {
+    const game = new Life(WIDTH, HEIGHT);
+    game.useObject(object, 10, 10);
+    cells = game.cells;
+
+    objectAddingOpen = false;
+  };
 </script>
 
-<!-- {#each cells as row, rowIndex}
-  <div class="flex">
-    {#each row as cell, cellIndex}
-      <div
-        on:click={() => {
-          cells[rowIndex][cellIndex] = !cells[rowIndex][cellIndex];
-          cells = cells;
-        }}
-        class="w-4 h-4"
-        class:bg-blue-400={cell}
-        class:bg-red-400={!cell}
-      />
-    {/each}
+{#if objectAddingOpen}
+  <div
+    class="fixed top-0 w-screen h-screen bg-gray-800 bg-opacity-40 z-40 flex items-center justify-center"
+  >
+    <div class="w-min-content flex flex-col gap-4">
+      {#each Object.entries(OBJECTS) as [name, object]}
+        <button class="p-2 bg-blue-200 cursor-pointer rounded-md" on:click={showObject(object)}>
+          {name}
+        </button>
+      {/each}
+
+      <button
+        class="p-2 bg-red-200 cursor-pointer rounded-md"
+        on:click={() => (objectAddingOpen = false)}
+      >
+        Cancel
+      </button>
+    </div>
   </div>
-{/each} -->
+{/if}
 
 <canvas
   width={WIDTH * SCALE}
@@ -186,6 +212,13 @@
 <div class="flex items-center justify-center absolute bottom-4 w-full">
   <div class="flex gap-2 bg-blue-400 p-1 rounded-md">
     <ToolbarButton Icon={ShuffleIcon} on:click={randomize} disabled={playing} />
+    <ToolbarButton
+      Icon={AddIcon}
+      on:click={() => {
+        objectAddingOpen = true;
+      }}
+      disabled={playing}
+    />
     <ToolbarButton
       Icon={playing ? PauseIcon : PlayIcon}
       on:click={switchPausePlay}
