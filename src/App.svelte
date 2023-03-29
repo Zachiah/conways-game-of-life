32e291792922099e1331f3f4cb79a0c5867b67f6
Zachiah Sawyer <zachiah@proton.me>
2023-03-29 14:16:04 -0700

    fix: add background to fps indicator

--- src/App.svelte
+++ src/App.svelte
@@ -239,7 +239,7 @@
 />
 
 {#if playing}
-  <div class="absolute top-4 right-4 font-mono">
+  <div class="absolute top-4 right-4 font-mono p-2 bg-white rounded-md opacity-70 hover:opacity-100">
     {$fps.toFixed(0)} FPS
   </div>
 {/if}
